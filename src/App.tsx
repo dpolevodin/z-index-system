1036fc5a0f909bf23f97d265e0d05230f39208db
dpolevodin <dpolevodin@gmail.com>
2025-02-14 19:30:59 +0600

    feat: added z-index with levels mixin

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,103 @@
 import {Toolbar} from './components/toolbar';
-import {DropdownMenu} from '@gravity-ui/uikit';
+import {DropdownMenu, HelpMark, PopupPlacement, type DropdownMenuItem} from '@gravity-ui/uikit';
 import '@gravity-ui/uikit/styles/fonts.css';
 import '@gravity-ui/uikit/styles/styles.css';
-import {DROPDOWN_ITEMS} from './constants';
+import './App.scss';
+
+const mdHelpPlacement: PopupPlacement = ['bottom', 'bottom-end', 'right-start', 'right', 'left'];
+
+const Help = () => (
+    <div
+        onClick={(e) => {
+            // stop clicks propagation
+            // because otherwise click in MarkdownHints handled as click on MenuItem
+            e.stopPropagation();
+        }}
+    >
+        <HelpMark popoverProps={{modal: false, placement: mdHelpPlacement}} className="mode-help">
+            <>
+                <div>test1</div>
+                <div>test2</div>
+                <div>test3</div>
+            </>
+        </HelpMark>
+    </div>
+);
+
+const DROPDOWN_ITEMS = [
+    {
+        text: <Help />,
+        action: () => console.log('Help'),
+    },
+    {
+        action: () => console.log('Rename'),
+        text: 'Rename',
+    },
+    {
+        action: () => console.log('Update'),
+        text: 'Update',
+    },
+    {
+        action: () => console.log('Delete'),
+        text: 'Delete',
+    },
+    {
+        action: () => console.log('Create'),
+        text: 'Create',
+    },
+    {
+        action: () => console.log('Rename'),
+        text: 'Rename',
+    },
+    {
+        action: () => console.log('Update'),
+        text: 'Update',
+    },
+    {
+        action: () => console.log('Delete'),
+        text: 'Delete',
+    },
+    {
+        action: () => console.log('Create'),
+        text: 'Create',
+    },
+    {
+        action: () => console.log('Rename'),
+        text: 'Rename',
+    },
+    {
+        action: () => console.log('Update'),
+        text: 'Update',
+    },
+    {
+        action: () => console.log('Delete'),
+        text: 'Delete',
+    },
+    {
+        action: () => console.log('Create'),
+        text: 'Create',
+    },
+    {
+        action: () => console.log('Rename'),
+        text: 'Rename',
+    },
+    {
+        action: () => console.log('Update'),
+        text: 'Update',
+    },
+    {
+        action: () => console.log('Delete'),
+        text: 'Delete',
+    },
+];
 
 function App() {
     return (
         <div>
             <Toolbar>
-                <DropdownMenu items={DROPDOWN_ITEMS} />
+                <div className="toolbar__dropdown">
+                    <DropdownMenu items={DROPDOWN_ITEMS} />
+                </div>
             </Toolbar>
         </div>
     );
